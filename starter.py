e43a5df36f177ab436de93490e003963312c2223
Sharwin24 <sharwin24@gmail.com>
2024-10-23 16:52:34 -0500

    Hamming distance and in_same_dimension function

--- starter.py
+++ starter.py
@@ -1,18 +1,47 @@
 
-def hamming(a, b):
+import numpy as np
+
+
+def in_same_dimension(a: np.ndarray, b: np.ndarray) -> bool:
+    """ Determines if the two given vectors are in the 
+        same dimension or not
+
+    Args:
+        a (np.ndarray): The first vector of any dimension
+        b (np.ndarray): The second vector of any dimension
+
+    Returns:
+        bool: Whether the 2 vectors are the same dimension or not
+    """
+    return np.shape(a) == np.shape(b)
+
+
+def hamming(a: np.ndarray, b: np.ndarray) -> int:
     """ Returns the Hamming distance between vectors a and b
 
     Args:
-        a (_type_): A vector
-        b (_type_): A vector
+        a (np.ndarray): A vector of any dimension
+        b (np.ndarray): A vector of any dimension
+
+    Returns:
+        int: The Hamming distance between the two vectors
+
+    Raises:
+        ValueError: If the given vectors are different dimensions
     """
+    # Ensure that the two vectors occupy the same dimension
+    if not in_same_dimension(a, b):
+        print(
+            f"Given vectors have different shapes: " +
+            f"{np.shape(a)} != {np.shape(b)}"
+        )
+        raise ValueError("Hamming requires 2 identically-shaped vectors")
     edits = 0
-    if len(a) != len(b):
-        print("Vectors must be the same length")
-        return -1
-    for i in range(len(a)):
-        if a[i] != b[i]:
-            edits += 1
+    rows, cols = np.shape(a)
+    for r in rows:
+        for c in cols:
+            if a[r][c] != b[r][c]:
+                edits += 1
     return edits
 
 
