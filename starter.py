741fc8e2ceb2874b65627dfdca00e607853dcef4
Sharwin24 <sharwin24@gmail.com>
2024-10-30 15:52:52 -0500

    KNN first try

--- starter.py
+++ starter.py
@@ -6,19 +6,24 @@ dist = 0
 MIN = 0
 MAX = 28
 # returns Euclidean distance between vectors and b
-def euclidean(a,b):
-    
-    return(dist)
-        
+
+
+def euclidean(a, b):
+
+    return (dist)
+
 # returns Cosine Similarity between vectors and b
-def cosim(a,b):
-    #Change to vectors
+
+
+def cosim(a, b):
+    # Change to vectors
     a = np.array(a)
     b = np.array(b)
-    #Generalize to higher dimensions
+    # Generalize to higher dimensions
     dist = np.dot(a, b) / np.sqrt(np.sum(a**2)) / np.sqrt(np.sum(b**2))
 
-    return(dist)
+    return (dist)
+
 
 def reduce(examples, r):
     # Step 1: Center the data (subtract the mean)
@@ -41,135 +46,171 @@ def reduce(examples, r):
 
     # Step 6: Project the data onto the top k eigenvectors
     X_reduced = np.dot(X_centered, top_eigenvectors)
-    
+
     return X_reduced
 
+
 def initialize_centroids(k):
-    
+
     clusters = []
-    
+
     for i in range(k):
         randx = np.random.randint(MIN, MAX)
         randy = np.random.randint(MIN, MAX)
         clusters.append((randx, randy))
-        
+
     return clusters
+
 
 def update_centroids():
     pass
-            
-# returns a list of labels for the query dataset based upon labeled observations in the train dataset.
-# metric is a string specifying either "euclidean" or "cosim".  
-# All hyper-parameters should be hard-coded in the algorithm.
-def knn(train,query,metric):
-    return(labels)
 
-# returns a list of labels for the query dataset based upon observations in the train dataset. 
-# labels should be ignored in the training set
-# metric is a string specifying either "euclidean" or "cosim".  
+# returns a list of labels for the query dataset based upon labeled observations in the train dataset.
+# metric is a string specifying either "euclidean" or "cosim".
 # All hyper-parameters should be hard-coded in the algorithm.
-def kmeans(train,query,metric):
+
+
+def knn(train: list, query: list, metric: str):
+    # For the given query, find the closest k examples in the training set
+    # Assign the most common label among those collected to that given query
+    # Do this for all queries
+    # query is list(pixels) 1-D array of length 784 (img_size)
+    # train is list[label, list(pixels)]
+    k = 5
+    labels = []
+    # Find k closest neighbors by sorting
+    f_d = None
+    if metric == 'euclidean':
+        f_d = euclidean
+    elif metric == 'cosim':
+        f_d = cosim
+    # Find the k closest neighbors ()
+    nearest_neighbors = sorted(
+        [t for t in train], key=lambda x: f_d(x[1], query))[:k]
+    # Find the most common label among the k closest neighbors
+    labels = [t[0] for t in nearest_neighbors]
+    # Find the most common label among the k closest neighbors
+    # and assign it to the query
+    most_common_label = max(set(labels), key=labels.count)
+    labels.append(most_common_label)
+    return (labels)
+
+# returns a list of labels for the query dataset based upon observations in the train dataset.
+# labels should be ignored in the training set
+# metric is a string specifying either "euclidean" or "cosim".
+# All hyper-parameters should be hard-coded in the algorithm.
+
+
+def kmeans(train, query, metric):
     k = 5
     centroids = initialize_centroids(k)
-    
+
     cluster_assignments = {}
-    
+
     for iter, example in enumerate(train):
         print("Iteration ", iter)
         assigned_centroid = None
-        
+
         for i in range(len(example)):
             pixel_x = i % MAX
             pixel_y = np.floor(i / 28)
             pixel_coords = (pixel_x, pixel_y)
-            
+
             min_dist = float('inf')
-            
+
             for centroid in centroids:
                 dist = cosim(pixel_coords, centroid)
                 if dist < min_dist:
                     min_dist = dist
                     assigned_centroid = centroid
-                    
-            if assigned_centroid != None:       
+
+            if assigned_centroid != None:
                 if assigned_centroid not in cluster_assignments:
                     cluster_assignments[assigned_centroid] = [pixel_coords]
                 else:
                     cluster_assignments[assigned_centroid].append(pixel_coords)
-                
-        #Return after first example to test
+
+        # Return after first example to test
         print("Intialized Centroids: ", centroids)
-        print("Cluster assignments for first example (not reduced): ", cluster_assignments)
-        print("Number of keys in cluster assignments: ", len(cluster_assignments.keys()))
+        print("Cluster assignments for first example (not reduced): ",
+              cluster_assignments)
+        print("Number of keys in cluster assignments: ",
+              len(cluster_assignments.keys()))
         return cluster_assignments
-    #return(labels)
+    # return(labels)
+
 
 def read_data(file_name):
-    
+
     data_set = []
-    with open(file_name,'rt') as f:
+    with open(file_name, 'rt') as f:
         for line in f:
-            line = line.replace('\n','')
+            line = line.replace('\n', '')
             tokens = line.split(',')
             label = tokens[0]
             attribs = []
             for i in range(784):
                 attribs.append(tokens[i+1])
-            data_set.append([label,attribs])
-    return(data_set)
-        
-def show(file_name,mode):
-    
+            data_set.append([label, attribs])
+    return (data_set)
+
+
+def show(file_name, mode):
+
     data_set = read_data(file_name)
     for obs in range(len(data_set)):
         for idx in range(784):
             if mode == 'pixels':
                 if data_set[obs][1][idx] == '0':
-                    print(' ',end='')
+                    print(' ', end='')
                 else:
-                    print('*',end='')
+                    print('*', end='')
             else:
-                print('%4s ' % data_set[obs][1][idx],end='')
+                print('%4s ' % data_set[obs][1][idx], end='')
             if (idx % 28) == 27:
                 print(' ')
-        print('LABEL: %s' % data_set[obs][0],end='')
+        print('LABEL: %s' % data_set[obs][0], end='')
         print(' ')
-            
+
+
 def main():
-    #show('valid.csv','pixels')
+    # show('valid.csv','pixels')
     r = 250
     k = 5
-    
-    #Testing distance metrics
-    a = np.array([1,2])
-    b = np.array([3,4])
+
+    mnist_training_data = read_data("mnist_train.csv")
+    mnist_testing_data = read_data("mnist_test.csv")
+    mnist_validation_data = read_data("mnist_valid.csv")
+    print(
+        f"Training data: {len(mnist_training_data)} Testing data: {len(
+            mnist_testing_data)} Validation data: {len(mnist_validation_data)}"
+    )
+    # Training data is a list of lists
+    # [[label, [pixels]]
+
+    # Testing distance metrics
+    a = np.array([1, 2])
+    b = np.array([3, 4])
     cosim(a, b)
-    
+
     data = pd.read_csv("mnist_train.csv")
-    
+
     print("Shape of data: ", data.shape)
-    
-    X = data.drop(data.columns[0], axis=1) #first column is class
-    X = data.drop(data.columns[-1], axis=1) #last column is nan?
-    y = data[data.columns[0]] #labels
-    
+
+    X = data.drop(data.columns[0], axis=1)  # first column is class
+    X = data.drop(data.columns[-1], axis=1)  # last column is nan?
+    y = data[data.columns[0]]  # labels
+
     X = np.array(X)
-    
+
     print("Shape of original X: ", X.shape)
     print("Shape of original y: ", y.shape)
-    
-    cluster_assignments_1 = kmeans(X[:500], None, None) #Only the first 500 examples
-    
-    
-    
-    
 
-    
+    # Only the first 500 examples
+    # cluster_assignments_1 = kmeans(X[:500], None, None)
+
+
     # X_reduced = reduce(X, r)
-
     # print("X Reduced shape:", X_reduced.shape)
-    
-    
 if __name__ == "__main__":
     main()
-    
