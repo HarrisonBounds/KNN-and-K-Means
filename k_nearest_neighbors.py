dfc2072b1735fa63690bbf17ffaa58076b1439fb
Sharwin Patil <sharwin24@gmail.com>
2024-11-01 18:49:14 -0500

    wrote some notes about TODO

--- k_nearest_neighbors.py
+++ k_nearest_neighbors.py
@@ -1,4 +1,5 @@
 from distance_metrics import euclidean, cosim
+from starter import read_data
 import numpy as np
 # returns a list of labels for the query dataset based upon observations in the train dataset.
 # labels should be ignored in the training set
@@ -43,3 +44,24 @@ def knn(train: list, query: list, metric: str, k: int = 5) -> list:
         )
         labels.append(most_common_label)
     return labels
+
+
+def run_knn():
+    mnist_training_data = read_data("mnist_train.csv")
+    mnist_testing_data = read_data("mnist_test.csv")
+    mnist_validation_data = read_data("mnist_valid.csv")
+    print(
+        f'Training Data Size: {len(mnist_training_data)}\n' +
+        f'Testing Data Size: {len(mnist_testing_data)}\n' +
+        f'Validation Data Size: {len(mnist_validation_data)}\n'
+    )
+    # Run training data through KNN and receive the labels for each query
+    # May have to modify KNN so the query is [label, list(pixels)] instead of just list(pixels)
+    # so that we can compare the assigned label to the actual label
+    # Not actually sure if this is how we do this
+
+    # Create a confusion matrix which shows the number of correct and incorrect labels
+    # True positive, true negative, false positive, false negative
+    # We need to do so for each label so we should have a 10x10 matrix
+    # Use the confusion matrix to calculate:
+    # Accuracy, Precision, Recall, F1 Score
